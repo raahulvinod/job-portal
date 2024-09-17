b78bd365ca4f28bec1cd1b448faf1118ba96bdd9
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-09-17 11:43:57 +0530

    feat: add router setups and pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+
+import router from './Router/Router.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <RouterProvider router={router} />
+);
