28257e7791522ab6f20244449bc3dccd9f48b1fc
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-09-17 18:53:49 +0530

    feat(client): add banner section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { useState } from 'react';
+
+import Banner from '../components/Banner';
 
 const Home = () => {
-  return <div className="text-blue">Home</div>;
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  return (
+    <div>
+      <Banner query={query} handleInputChange={handleInputChange} />
+    </div>
+  );
 };
 
 export default Home;
