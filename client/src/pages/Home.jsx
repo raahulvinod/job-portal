664322e19f9f25e5d19cb6c4a7ad6c8a8067e267
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-09-18 13:58:19 +0530

    feat(client): showing number of jobs

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -79,7 +79,23 @@ const Home = () => {
         </div>
         {/*  Job cards */}
         <div className="col-span-2 bg-white p-4 rounded">
-          {isLoading ? <p>Loading...</p> : <Jobs result={result} />}
+          {isLoading ? (
+            <div
+              className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+              role="status"
+            >
+              <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                Loading...
+              </span>
+            </div>
+          ) : result.length > 0 ? (
+            <Jobs result={result} />
+          ) : (
+            <>
+              <h3 className="text-lg font-bold mb-4">{result.length} Jobs</h3>
+              <p>No jobs found</p>
+            </>
+          )}
         </div>
         <div className="bg-white p-4 rounded"></div>
       </div>
--- client/src/pages/Jobs.jsx
+++ client/src/pages/Jobs.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Jobs = ({ result }) => {
-  return <div>{result}</div>;
+  return (
+    <>
+      <div>
+        <h3 className="text-lg font-bold mb-4">{result.length} Jobs</h3>
+        <h3></h3>
+      </div>
+      <section>{result}</section>
+    </>
+  );
 };
 
 export default Jobs;
